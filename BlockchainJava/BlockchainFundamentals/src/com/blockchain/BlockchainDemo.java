bb4d8de97911f9f3528db9387964f7ddb50258c8
Anubhav Paras <32901101+anubhp@users.noreply.github.com>
2018-05-19 16:20:12 +0530

    Update BlockchainDemo.java

--- BlockchainJava/BlockchainFundamentals/src/com/blockchain/BlockchainDemo.java
+++ BlockchainJava/BlockchainFundamentals/src/com/blockchain/BlockchainDemo.java
@@ -10,20 +10,20 @@ public class BlockchainDemo {
 	
 	public static void main (String []args){
 		Blockchain myBlockchain = new Blockchain("Ripple", 2, 75);
-		myBlockchain.createTransaction(new Transaction("Rob", "Sam", 10));
-		myBlockchain.createTransaction(new Transaction("Joseph", "Rob", 20));
+		myBlockchain.createTransaction(new Transaction("Jeff", "Salil", 10));
+		myBlockchain.createTransaction(new Transaction("Alisha", "Raj", 20));
 		
 		System.out.println("\nMy Blockchain: \n" + myBlockchain);
 		
-		myBlockchain.minePendingTransactions("Rob");
+		myBlockchain.minePendingTransactions("Salil");
 		
 		System.out.println("\nMy Blockchain after mining the blocks: \n" + myBlockchain);
 		
-		myBlockchain.minePendingTransactions("Rob");
+		myBlockchain.minePendingTransactions("Salil");
 		
 		System.out.println("\nMy Blockchain after mining the blocks second time: \n" + myBlockchain);
 		
-		System.out.println("\nBalance of Rob: " + myBlockchain.getBalanceofNode("Rob"));
+		System.out.println("\nBalance of Salil: " + myBlockchain.getBalanceofNode("Salil"));
 		
 	}
 
